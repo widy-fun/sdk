dfdc43b082f9661e0fcdbc06f970757f9e5e3d7c
ik1s3v <ik1s3v@gmail.com>
2026-07-01 12:18:42 +0300

    feat: add widy app theme

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./enums";
 export * from "./rsBuildHotReloadPlugin";
+export * from "./theme";
 export * from "./types";
 export { default as WidgetOutboundBridge } from "./WidgetOutboundBridge";
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+export const darkTheme = {
+	palette: {
+		mode: "dark",
+		primary: {
+			main: "#1976d2",
+			contrastText: "#ffffff",
+		},
+		switchOff: {
+			text: "#838383",
+		},
+		background: {
+			default: "#0e1621",
+			paper: "#182533",
+			section: "#17212b",
+		},
+	},
+};
